304b86e5ab041929641601d8a36e818087fd2ab8
Nour Saeed <nour.saeed@gmail.com>
2018-02-18 18:37:54 +0100

    Add: MessageConfigurations class.

---
+++ MessageLibrary/MessageConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageLibrary
+{
+    public class MessageConfigurations
+    {
+        public MessageType _Type = MessageType.None;
+        public MessageDestination _Destination = MessageDestination.None;
+        public MessageConfigurations Log = new MessageConfigurations();
+
+        public MessageType Type
+        {
+            get => _Type;
+            set
+            {
+                _Type = value;
+
+                if (Log != null)
+                    Log.Type = _Type;
+            }
+        }
+
+        public MessageConfigurations()
+        {
+            Type = MessageType.None;
+        }
+    }
+}
